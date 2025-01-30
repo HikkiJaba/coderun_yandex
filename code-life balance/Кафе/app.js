b81ac376df5cc631b6ff138efec9696462f8ad2e
HikkiJaba <brait.2005@mail.ru>
2025-01-31 01:56:46 +0700

    fix cafe

--- code-life balance/Кафе/app.js
+++ code-life balance/Кафе/app.js
@@ -12,7 +12,6 @@ rl.on('line', line => {
     if (!line) {
         
         if(arr.length != 0) {
-            console.log("Вывод при выходе ", arr);
             getPrice();
         } else {
             console.log("Массив пустой");
@@ -28,7 +27,26 @@ rl.on('line', line => {
 
 function getPrice() {
     let cupon = 0;
+    let cost_eat = 0;
     let use_cupon = 0;
-
-    while (num)
+    let day_use = 0;
+    let num = 1;
+    while (num <= arr[0]) {
+        if (arr[num] > 100) {
+            cupon += 1;
+        } else {
+            if(cupon) {
+                use_cupon += 1;
+                cupon -= 1;
+                num += 1;
+                day_use = num;
+                continue;
+            }
+        }
+        cost_eat += Number(arr[num]);
+        num += 1;
+    }
+    console.log (cost_eat);
+    console.log (cupon, use_cupon);
+    console.log (day_use);
 }
